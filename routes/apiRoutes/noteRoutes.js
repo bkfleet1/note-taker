db80371e95a792f9f21497689b7c1fa4e602d045
bkfleet1 <bkfleet1@gmail.com>
2022-02-10 13:45:17 -0600

    GET & PUSH /api/notes routes added; UUID added to new notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const router = express.Router();
+const router = require("express").Router();
+const { getNotes, newNote, validateNote } = require("../../lib/notes");
+// const db = require("../../db/db");
 
 
-router.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-  
+router.get("/notes", (req, res) => {
+  let results = getNotes();
+  res.json(results);
+});
 
 
+router.post("/notes", (req, res) => {
+  // x = db.length.toString()
+  if (!validateNote(req.body)) {
+    res.status(400).send('Opps! Something went wrong. The Note information you added was incomplete.');
+  } else {
+    const note = newNote(req.body);
+    res.json(note);
+  }
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes'); // API ROUTES
+// const htmlRoutes = require('./routes/htmlRoutes'); // HTML ROUTES
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
+// MIDDLEWARE to parse incoming string or array data. Must include to POST data.
+app.use(express.static('public')); // Makes the public path static
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
 
 
-// Use apiRoutes
+// BROWSER ROUTE TO APIs AND PAGES
 app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
