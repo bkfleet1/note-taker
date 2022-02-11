1e91428ac8b35aad67632e4f44c99048ba0befe6
bkfleet1 <bkfleet1@gmail.com>
2022-02-10 19:04:12 -0600

    notes working

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const path = require('path');
 
 // API ROUTES FOR SITE PAGES
-router.get('/', (req, res) => {
+router.get('/notes', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../../public/notes.html'));
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    // res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
