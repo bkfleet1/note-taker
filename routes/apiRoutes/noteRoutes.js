42eaa4ed1cbadf103e3481a2e3b4296c5e2f0d30
bkfleet1 <bkfleet1@gmail.com>
2022-02-10 18:20:36 -0600

    delete APU added

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getNotes, newNote, validateNote, getNoteById, deleteNoteById } = require("../../lib/notes");
+const { getNotes, getNote, validateNote, saveNote, deleteNote } = require("../../lib/notes");
 // const db = require("../db/db");
 
 
@@ -9,7 +9,7 @@ router.get("/notes", (req, res) => {
 });
 
 router.get("/notes/:id", (req, res) => {
-  let results = getNoteById(req.params.id);
+  let results = getNote(req.params.id);
   if (results) {
   res.json(results);
   } else {
@@ -22,18 +22,18 @@ router.post("/notes", (req, res) => {
   if (!validateNote(req.body)) {
     res.status(400).send('Opps! Something went wrong. The Note information you added was incomplete.');
   } else {
-    const note = newNote(req.body);
+    const note = saveNote(req.body);
     res.json(note);
   }
 });
 
 // DELETE route to remove note by id.
-router.delete('/voter/:id', (req, res) => {
-  let results = deleteNoteById(req.params.id);
+router.delete('/notes/:id', (req, res) => {
+  let results = deleteNote(req.params.id);
   if (results) {
   res.json(results);
   } else {
-    res.send(404);
+    res.status(404).send('Opps! Something went wrong.');
   }
 });  
 
